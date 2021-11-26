87e1746ec6a1334520eb1e9eaa86cbe3c781f552
Daniel Obisanya <ifeoluwadaniel25@gmail.com>
2021-11-26 22:18:10 +0100

    Edited

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Auxi from "./../../hoc/Auxi";
-import classes from "./Layout.css";
+import Auxi from "../../hoc/Auxi";
+import Great from "./Layout.module.css";
 
 const layout = (props) => (
   <Auxi>
     <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main className={classes.content}>{props.children}</main>
+    <main className={Great.content}>{props.children}</main>
   </Auxi>
 );
 
